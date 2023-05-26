2475f06151637b0e0118591b0bd3fd749498ead6
mehran108 <mehrankhan108@hotmail.com>
2023-05-26 23:53:33 +0500

    release added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,11 +2,9 @@ const env = 'http://mehran108-001-site1.htempurl.com/api';
 export const environment = {
   production: true,
   appRoot: 'http://mehran108-001-site1.htempurl.com/api',
-  appGroup: `${env}/Groups`,
-  appList: `${env}/List`,
-  appUser: `${env}/users`,
-  appStudent: `${env}/Student`,
-  appFile: `${env}/FileManager`,
-  appReporting: `${env}/Reporting`,
+  appCustomer: `${env}/Customer`,
+  appCustomerMeasurement: `${env}/CustomerMeasurement`,
+  appOrder: `${env}/Order`,
+  appUser: `${env}/User`,
   appURL: `http://db.elicamps.com/`
 };
